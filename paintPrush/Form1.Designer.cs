23d036665f42a16713ac0b4d2f531372ec17b2ae
hp <hp@DESKTOP-42SEDR9>
2022-02-05 20:40:03 +0200

    Add project files.

---
+++ paintPrush/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace paintBrush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnPenColor = new System.Windows.Forms.Button();
+            this.lblPenColor = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCanvesColor = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCoordinates = new System.Windows.Forms.TextBox();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(956, 95);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(425, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Paint";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SlateGray;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnSquare);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.btnLine);
+            this.panel2.Controls.Add(this.btnCircle);
+            this.panel2.Controls.Add(this.btnRectangle);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 95);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 553);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnPenColor);
+            this.panel7.Controls.Add(this.lblPenColor);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel7.Location = new System.Drawing.Point(0, 306);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(198, 82);
+            this.panel7.TabIndex = 9;
+            // 
+            // btnPenColor
+            // 
+            this.btnPenColor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPenColor.Location = new System.Drawing.Point(3, 32);
+            this.btnPenColor.Name = "btnPenColor";
+            this.btnPenColor.Size = new System.Drawing.Size(190, 35);
+            this.btnPenColor.TabIndex = 10;
+            this.btnPenColor.UseVisualStyleBackColor = false;
+            this.btnPenColor.Click += new System.EventHandler(this.btnPenColor_Click);
+            // 
+            // lblPenColor
+            // 
+            this.lblPenColor.AutoSize = true;
+            this.lblPenColor.Location = new System.Drawing.Point(55, 9);
+            this.lblPenColor.Name = "lblPenColor";
+            this.lblPenColor.Size = new System.Drawing.Size(78, 20);
+            this.lblPenColor.TabIndex = 11;
+            this.lblPenColor.Text = "Pen Color";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Controls.Add(this.btnCanvesColor);
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 388);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(198, 85);
+            this.panel5.TabIndex = 8;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.button1);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(198, 85);
+            this.panel6.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(5, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 35);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Canves Color";
+            // 
+            // btnCanvesColor
+            // 
+            this.btnCanvesColor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCanvesColor.Location = new System.Drawing.Point(5, 38);
+            this.btnCanvesColor.Name = "btnCanvesColor";
+            this.btnCanvesColor.Size = new System.Drawing.Size(190, 35);
+            this.btnCanvesColor.TabIndex = 9;
+            this.btnCanvesColor.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Canves Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Draw Shapes";
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnSquare.Location = new System.Drawing.Point(-2, 35);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(200, 67);
+            this.btnSquare.TabIndex = 7;
+            this.btnSquare.Text = "Square";
+            this.btnSquare.UseVisualStyleBackColor = false;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.txtCoordinates);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 473);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(198, 78);
+            this.panel4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Location";
+            // 
+            // txtCoordinates
+            // 
+            this.txtCoordinates.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtCoordinates.Enabled = false;
+            this.txtCoordinates.Location = new System.Drawing.Point(14, 34);
+            this.txtCoordinates.Multiline = true;
+            this.txtCoordinates.Name = "txtCoordinates";
+            this.txtCoordinates.Size = new System.Drawing.Size(163, 36);
+            this.txtCoordinates.TabIndex = 0;
+            // 
+            // btnLine
+            // 
+            this.btnLine.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnLine.Location = new System.Drawing.Point(0, 240);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(200, 75);
+            this.btnLine.TabIndex = 3;
+            this.btnLine.Text = "Line ";
+            this.btnLine.UseVisualStyleBackColor = false;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnCircle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCircle.Location = new System.Drawing.Point(-1, 168);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(200, 75);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = false;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnRectangle.Location = new System.Drawing.Point(-1, 98);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(200, 76);
+            this.btnRectangle.TabIndex = 1;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = false;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(200, 95);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(756, 553);
+            this.panel3.TabIndex = 2;
+            this.panel3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseClick);
+            this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::paintPrush.Properties.Resources.icons8_paint_brush_64;
+            this.pictureBox1.Location = new System.Drawing.Point(341, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.SlateGray;
+            this.button2.Location = new System.Drawing.Point(854, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 58);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(956, 648);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Paint Brush Application";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtCoordinates;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnCanvesColor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnPenColor;
+        private System.Windows.Forms.Label lblPenColor;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ paintPrush/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paintBrush
+{
+    
+    public partial class Form1 : Form
+    {
+        private Point point1;
+        private Point point2;
+        Graphics Graphics;
+        bool startPaint = false;
+        Pen pen;
+        bool Square = false;
+        bool Rectangle = false;
+        bool Circle = false;
+        bool Line = false;
+        Color color = Color.Black;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        { 
+        }
+
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            string coordinates = e.X + "," + e.Y;
+            txtCoordinates.Text = coordinates;
+        }
+        private void btnPenColor_Click(object sender, EventArgs e)
+        {
+            colorDialog2.ShowDialog();
+            color = colorDialog2.Color;
+            btnPenColor.BackColor = colorDialog2.Color;
+        }
+        private void panel3_MouseClick(object sender, MouseEventArgs e)
+        {
+            pen = new Pen(color);
+            Graphics = panel3.CreateGraphics();
+            if (startPaint == false)
+            {
+                point1 = new Point(e.X, e.Y);
+                startPaint = true;
+            }
+           else if (Square == true && startPaint == true)
+           { 
+                point2 = new Point(e.X, e.Y);
+                int width = Math.Abs(point1.X - point2.X);
+                int height = Math.Abs(point1.Y - point2.Y);
+
+                Rectangle rectangle = new Rectangle(point1.X, point1.Y, width, width);
+                Graphics.DrawRectangle(pen, rectangle);
+                
+                startPaint = false;
+                Square = false;
+           }
+            else if (Rectangle == true && startPaint == true)
+            {
+               
+                point2 = new Point(e.X, e.Y);
+                int width = Math.Abs(point1.X - point2.X);
+                int height = Math.Abs(point1.Y - point2.Y);
+
+                Rectangle rectangle = new Rectangle(point1.X, point1.Y, width, height);
+                Graphics.DrawRectangle(pen, rectangle);
+               
+                startPaint = false;
+                Rectangle = false;
+            }
+            else if (Circle == true && startPaint == true)
+            {
+                
+               
+                point2 = new Point(e.X, e.Y);
+                int width = Math.Abs(point1.X - point2.X);
+                int height = Math.Abs(point1.Y - point2.Y);
+
+                Rectangle rectangle = new Rectangle(point1.X, point1.Y, width, height);
+                Graphics.DrawEllipse(pen,rectangle);
+                startPaint = false;
+                Circle = false;
+            }
+            else if(Line == true && startPaint == true)
+            {
+               
+              
+                point2 = new Point(e.X, e.Y);
+
+
+                Graphics.DrawLine(pen, point1, point2);
+                Graphics.DrawLine(pen, point2, point1);
+
+                startPaint = false;
+                Line = false;
+            }
+        }
+       
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            Square = true;
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            Rectangle = true;
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            Circle = true;
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            Line = true;
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            panel3.BackColor = colorDialog1.Color;
+            button1.BackColor = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel3.Controls.Clear();
+            this.Refresh();
+            panel3.BackColor = Color.White;
+            btnCanvesColor.BackColor = Color.White;
+        }
+    }
+}
